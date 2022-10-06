e57472148786c02e6c5e8cb7e43c1819f48d64e0
Danval-003 <77825594+Danval-003@users.noreply.github.com>
2022-10-06 07:43:36 -0600

    Nueva Branch

---
+++ cmake-build-debug/CMakeFiles/Proyecto2Grupo5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto2Grupo5.dir/main.cpp.obj"
+  "CMakeFiles/Proyecto2Grupo5.dir/main.cpp.obj.d"
+  "Proyecto2Grupo5.exe"
+  "Proyecto2Grupo5.exe.manifest"
+  "Proyecto2Grupo5.pdb"
+  "libProyecto2Grupo5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto2Grupo5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Proyecto2Grupo5.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Proyecto2Grupo5.
+# This may be replaced when dependencies are built.
